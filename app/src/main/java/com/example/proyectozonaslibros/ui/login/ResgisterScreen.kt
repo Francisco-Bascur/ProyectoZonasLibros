993851c39e838eca2c855460a63519103263358d
Francisco-Bascur <fr.bascur@duocuc.cl>
2025-10-31 16:29:27 -0300

    se crea pantalla basica de RegisterScreen con texto (sin navegacion)

---
+++ app/src/main/java/com/example/proyectozonaslibros/ui/login/ResgisterScreen.kt
@@ -0,0 +1,33 @@
+package com.example.proyectozonaslibros.ui.login
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RegisterScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Pantalla de Registro")
+
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun RegisterScreenPreview() {
+    RegisterScreen()
+}
